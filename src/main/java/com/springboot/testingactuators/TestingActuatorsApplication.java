84fa4050e6294f501160942d9f33e492fff4f930
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-02-04 09:03:54 +0800

    first commit with files

---
+++ src/main/java/com/springboot/testingactuators/TestingActuatorsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.testingactuators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingActuatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingActuatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/testingactuators/TestingactuatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.testingactuators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingactuatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
